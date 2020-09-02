3491613111839ebb7cf971b1721b2669d4e59347
Joao Felipe <felipe.joao@uol.com.br>
2020-09-02 19:09:14 -0300

    try to fix upload image button

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,7 @@ class User < ApplicationRecord
 
   has_many :products, dependent: :destroy
   has_many :orders
+  has_one_attached :photo
 
   validates :email, presence: true, uniqueness: true
   validates :password, :first_name, :last_name, :address, presence: true

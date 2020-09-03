7f55737b7dac4b111bc989c7d60b822818da9b6b
Joao Felipe <felipe.joao@uol.com.br>
2020-09-03 11:58:42 -0300

    fixed upload imagem buttom

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -47,6 +47,6 @@ class ProductsController < ApplicationController
   private
 
   def product_params
-    params.require(:product).permit(:name, :description, :gender, :size, :brand, :category, :price)
+    params.require(:product).permit(:name, :description, :gender, :size, :brand, :category, :price, :photo)
   end
 end

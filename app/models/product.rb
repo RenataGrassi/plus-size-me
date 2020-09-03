46b3ce1656af376318f12f7058cf3f0df5e12a05
Joao Felipe <felipe.joao@uol.com.br>
2020-09-03 14:49:37 -0300

    upload image created but need to increment more style

--- app/models/product.rb
+++ app/models/product.rb
@@ -7,7 +7,7 @@ class Product < ApplicationRecord
   CATEGORY = ["shirts", "pants", "dresses", "skirts", "shoes", "jackets"]
 
 
-  validates :name, :gender, :size, :category, :price, presence: true
+  validates :name, :photo, :gender, :size, :category, :price, presence: true
   validates :gender, inclusion: { in: GENDER }
   validates :size, inclusion: { in: SIZE }
   validates :category, inclusion: { in: CATEGORY }
